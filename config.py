d2069a8724dad258452a71f5f7b291ce068ec496
Daniel Rodrigues Amorim <105808962+Pintass@users.noreply.github.com>
2024-12-30 12:20:13 +0000

    maj 30/12
    suite graphic side + ajout d'images + responsive par rapport à la taille des cases
    la map est générée correctement avec des couleurs pour le moment en attendant des images, chaque case importante a ses cords stockées dans une liste remplie de coordonnées, il y a aussi une inversion entre les paramètres et la map du .txt

--- config.py
+++ config.py
@@ -3,6 +3,6 @@ LONGUEUR = 21
 LARGEUR = 20
 
 # graphic side
-longueurfenetre = 800
-largeurfenetre = 600
 taillecase = 20
+longueurfenetre = taillecase*21
+largeurfenetre = taillecase*21
--- graphicside.py
+++ graphicside.py
@@ -2,31 +2,58 @@ from tkiteasy import ouvrirFenetre
 import config as cfg
 
 g = ouvrirFenetre(cfg.longueurfenetre, cfg.largeurfenetre)
+cord_col = []
+cord_mur = []
+cord_eth = []
 
-# début du code
+
+# déclaration des fonctions
 
 
 def creation_carte():
     """
     permet de créer la carte de façon graphique grâce à la carte du terminale en plus d'un damier.
     """
-    compteur = 0
-    for l in range(0, cfg.longueurfenetre, cfg.taillecase):
-        for h in range(0, cfg.largeurfenetre, cfg.taillecase):
-            if compteur % 2 == 0:
-                g.dessinerRectangle(l, h, cfg.taillecase, cfg.taillecase, "#578c8a")
+    x = 0
+    y = 0
+    file  = open("map0.txt","r")
+    lignes = file.readlines()
+    for ligne in range(len(lignes)) :
+        for mot in range(len(lignes[ligne])):
+            x = mot*cfg.taillecase
+            y = ligne*cfg.taillecase
+            if lignes[ligne][mot] == "C":
+                g.dessinerRectangle(x, y, cfg.taillecase, cfg.taillecase, "grey")
+                cord_col.append([x, y])
+            elif lignes[ligne][mot] == "M":
+                g.dessinerRectangle(x, y, cfg.taillecase, cfg.taillecase, "black")
+                cord_mur.append([x, y])
+            elif lignes[ligne][mot] == "E":
+                g.dessinerRectangle(x, y, cfg.taillecase, cfg.taillecase, "blue")
+                cord_eth.append([x, y])
             else:
-                g.dessinerRectangle(l, h, cfg.taillecase, cfg.taillecase, "#b2fffd")
-            compteur += 1
-        compteur += 1
+                g.dessinerRectangle(x, y, cfg.taillecase, cfg.taillecase, "green")
+    file.close()
 
-        file  = open("map0.txt","r")
-        lignes = file.readlines()
-        for ligne in lignes :
-            print(ligne)
-            # if ligne 
-        file.close()
+def case_valide(x:int,y:int):
+    """
+    permet de savoir si une case est valide par ses coordonnées x et y
 
+    Args:
+    x(int) : position horizontale de la case
+    y(int) : position verticale de la case
+    
+    Returns:
+    True si elle est disponible
+    False si la case est occupée ou infranchissable
+    """
+    couleurs_invalides = ["black", "grey", "blue"]
+    # if getcouleur
+
+
+
+
+# jeu
 creation_carte()
 
 while g.recupererClic() is None:
